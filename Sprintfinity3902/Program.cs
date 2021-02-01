4f186c68a472ea5dc0765b3c3db44d86a895e581
Kyle Gessel <44045020+kylegessel@users.noreply.github.com>
2021-02-01 10:42:36 -0500

    Add project files.

---
+++ Sprintfinity3902/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Sprintfinity3902
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}

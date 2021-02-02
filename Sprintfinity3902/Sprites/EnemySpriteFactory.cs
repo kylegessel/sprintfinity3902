4dbc75d9849451daae63c25a3a79da9f4694ff9c
Bailey-Ardrey <59753201+Bailey-Ardrey@users.noreply.github.com>
2021-02-02 16:19:42 -0500

    Set up EnemySpriteFactory as a Singleton object.

--- Sprintfinity3902/Sprites/EnemySpriteFactory.cs
+++ Sprintfinity3902/Sprites/EnemySpriteFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,22 @@ namespace Sprintfinity3902.Sprites
 {
     public class EnemySpriteFactory
     {
+        private Texture2D enemySpritesheet;
+
+        private static EnemySpriteFactory instance = new EnemySpriteFactory();
+
+        public static EnemySpriteFactory Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        private EnemySpriteFactory()
+        {
+
+        }
 
     }
 }

e425e7ae00cc99538f283f02fa6515f6c58f9d4d
Bailey-Ardrey <59753201+Bailey-Ardrey@users.noreply.github.com>
2021-02-02 18:39:43 -0500

    Created properties and methods for the Enemy class.

--- Sprintfinity3902/Enemy.cs
+++ Sprintfinity3902/Enemy.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Sprintfinity3902.Interfaces;
+using Sprintfinity3902.Sprites;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +8,20 @@ namespace Sprintfinity3902
 {
     public class Enemy
     {
+        private Game1 _Game;
+        public ISprite CurrentEnemy { get; set; }
+        public EnemySpriteFactory EnemyFactory { get; set; }
+
+        public Enemy(Game1 game)
+        {
+            _Game = game;
+            EnemyFactory = EnemySpriteFactory.Instance;
+        }
+
+        public void getEnemy()
+        {
+            CurrentEnemy = EnemyFactory.CreateBlueBatEnemy();
+        }
 
     }
 }

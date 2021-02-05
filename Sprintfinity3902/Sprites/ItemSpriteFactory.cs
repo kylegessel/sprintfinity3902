e95149928a18f65e6a73af75311637cca6c38ac1
Brad Knez <brad.knez@gmail.com>
2021-02-05 13:22:38 -0500

    Add item sprite factory.

---
+++ Sprintfinity3902/Sprites/ItemSpriteFactory.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Sprintfinity3902.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprintfinity3902.Sprites
+{
+    class ItemSpriteFactory
+    {
+        private Texture2D itemSpriteSheet;
+
+        private static ItemSpriteFactory instance = new ItemSpriteFactory();
+
+        public static ItemSpriteFactory Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        private ItemSpriteFactory()
+        {
+
+        }
+
+        public void LoadAllTextures(ContentManager content)
+        {
+            itemSpriteSheet = content.Load<Texture2D>("Zelda - Link and Items - Transparent");
+        }
+
+        public ISprite CreateBombItem()
+        {
+            return new BombItemSprite(itemSpriteSheet);
+        }
+    }
+}

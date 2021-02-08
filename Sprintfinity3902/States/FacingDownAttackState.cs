733536ad3ed5c27bffc4cc569a6297d125ea4ab6
Bailey-Ardrey <59753201+Bailey-Ardrey@users.noreply.github.com>
2021-02-08 13:30:26 -0500

    Make changes to Animation to work with Attack Command.

---
+++ Sprintfinity3902/States/FacingDownAttackState.cs
@@ -0,0 +1,33 @@
+﻿using Sprintfinity3902.Sprites;
+using Sprintfinity3902.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprintfinity3902.States
+{
+    public class FacingDownAttackState : IPlayerState
+    {
+        public ISprite Sprite { get; set; }
+        Player PlayerCharacter;
+
+        public FacingDownAttackState(Player playerCharacter)
+        {
+            PlayerCharacter = playerCharacter;
+            Sprite = new LinkDownAttackSprite(PlayerCharacter.PlayerTexture, PlayerCharacter.StartingLocation);
+            Sprite.GetAnimation();
+            if(Sprite.Animation.IsPlaying == false)
+            {
+                PlayerCharacter.setState(PlayerCharacter.facingDown);
+            }
+        }
+
+
+        public void Move()
+        {
+            //Sprite.CurrentPositionY = Sprite.CurrentPositionY + 5;
+            //PlayerCharacter.setCurrentPositionY(Sprite.CurrentPositionY);
+        }
+
+    }
+}

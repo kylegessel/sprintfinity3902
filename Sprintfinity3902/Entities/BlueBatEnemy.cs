fe46009c1431b445bca9bca9ba6157f0bf241f1c
Brad Knez <brad.knez@gmail.com>
2021-02-10 10:47:12 -0500

    Implement bat enemy.

--- Sprintfinity3902/Entities/BlueBatEnemy.cs
+++ Sprintfinity3902/Entities/BlueBatEnemy.cs
@@ -8,10 +8,69 @@ namespace Sprintfinity3902.Entities
 {
     public class BlueBatEnemy : AbstractEntity
     {
+
+        private Random rand = new Random();
+        private int count;
+        // For slowing down in future.
+        private int previousDirection;
+        private int direction;
+        private int waitTime;
+
         public BlueBatEnemy()
         {
             Sprite = EnemySpriteFactory.Instance.CreateBlueBatEnemy();
-            Position = new Vector2(500, 500);
+            Position = new Vector2(700, 500);
+
+            direction = 0;
+            count = 0;
+        }
+
+        public override void Move()
+        {
+            if(count == 0)
+            {
+                waitTime = rand.Next(500);
+            }else if( count == waitTime)
+            {
+                // States for left, right, up, down, up right, up left, down left, down right.
+                direction = rand.Next(1, 9);
+                count = 0;
+            }
+
+            switch (direction)
+            {
+                case 1:
+                    X = X - 2;
+                    break;
+                case 2:
+                    X = X + 2;
+                    break;
+                case 3:
+                    Y = Y - 2;
+                    break;
+                case 4:
+                    Y = Y + 2;
+                    break;
+                case 5:
+                    X = X + 1;
+                    Y = Y + 1;
+                    break;
+                case 6:
+                    X = X - 1;
+                    Y = Y + 1;
+                    break;
+                case 7:
+                    X = X - 1;
+                    Y = Y - 1;
+                    break;
+                case 8:
+                    X = X + 1;
+                    Y = Y - 1;
+                    break;
+                case 9:
+                    break;
+            }
+            count++;
         }
     }
 }

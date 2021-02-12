5ef32e24f5c3ab556a338bf120a03f5edd4582d2
Casey Vancauwenbergh <vancauwenbergh.3@osu.edu>
2021-02-11 22:34:50 -0500

    Create a Command Class to set Link to Damaged

---
+++ Sprintfinity3902/Commands/SetDamageLinkCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sprintfinity3902.Entities;
+using Sprintfinity3902.Interfaces;
+
+namespace Sprintfinity3902.Commands
+{
+    class SetDamageLinkCommand : ICommand
+    {
+        AbstractEntity PlayerCharacter;
+        
+
+        public SetDamageLinkCommand(Player player, Game1 game)
+        {
+            PlayerCharacter = player;
+            PlayerCharacter = new DamagedLink(player, game);
+            
+        }
+
+        public void Execute()
+        {
+            
+        }
+    }
+}

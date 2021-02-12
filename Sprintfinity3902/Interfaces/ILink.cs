ff08b639605ea5877c265a1e8e5111629090b2b6
Casey Vancauwenbergh <vancauwenbergh.3@osu.edu>
2021-02-11 19:36:37 -0500

    Add ILink Interface

---
+++ Sprintfinity3902/Interfaces/ILink.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Sprintfinity3902.Sprites;
+
+namespace Sprintfinity3902.Interfaces
+{
+    interface ILink
+    {
+        ISprite Sprite
+        {
+            get; set;
+        }
+        Vector2 Position
+        {
+            get; set;
+        }
+        int X
+        {
+            get; set;
+        }
+        int Y
+        {
+            get; set;
+        }
+        void Update(GameTime gameTime);
+        void Draw(SpriteBatch spriteBatch);
+        void Move();
+        void SetState(IPlayerState state);
+        void TakeDamage();
+        void RemoveDecorator();
+    }
+}
+

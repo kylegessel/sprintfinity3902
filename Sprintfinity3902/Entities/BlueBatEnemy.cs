35cb158b8cb7105cebdafb40e2651e4c80fd1a04
Brad Knez <brad.knez@gmail.com>
2021-02-12 13:48:12 -0500

    Implement basic BlueBatEnemy.

--- Sprintfinity3902/Entities/BlueBatEnemy.cs
+++ Sprintfinity3902/Entities/BlueBatEnemy.cs
@@ -11,16 +11,13 @@ namespace Sprintfinity3902.Entities
 
         private Random rand = new Random();
         private int count;
-        // For slowing down in future.
-        private int previousDirection;
         private int direction;
         private int waitTime;
 
         public BlueBatEnemy()
         {
             Sprite = EnemySpriteFactory.Instance.CreateBlueBatEnemy();
-            Position = new Vector2(700, 500);
-
+            Position = new Vector2(750, 540);
             direction = 0;
             count = 0;
         }
@@ -29,12 +26,17 @@ namespace Sprintfinity3902.Entities
         {
             if(count == 0)
             {
-                waitTime = rand.Next(500);
+                waitTime = rand.Next(200);
             }else if( count == waitTime)
             {
                 // States for left, right, up, down, up right, up left, down left, down right.
-                direction = rand.Next(1, 9);
-                count = 0;
+               direction = rand.Next(1, 9);
+                // If the Sprite animation was previously stopped, begin playing it again.
+                if (!Sprite.Animation.IsPlaying) {
+                    Sprite.Animation.Play();
+                }
+
+               count = 0;
             }
 
             switch (direction)
@@ -52,25 +54,29 @@ namespace Sprintfinity3902.Entities
                     Y = Y + 2;
                     break;
                 case 5:
-                    X = X + 1;
-                    Y = Y + 1;
+                    X = X + 2;
+                    Y = Y + 2;
                     break;
                 case 6:
-                    X = X - 1;
-                    Y = Y + 1;
+                    X = X - 2;
+                    Y = Y + 2;
                     break;
                 case 7:
-                    X = X - 1;
-                    Y = Y - 1;
+                    X = X - 2;
+                    Y = Y - 2;
                     break;
                 case 8:
-                    X = X + 1;
-                    Y = Y - 1;
+                    X = X + 2;
+                    Y = Y - 2;
                     break;
                 case 9:
+                    // Stop animation when not moving.
+                    Sprite.Animation.Stop();
                     break;
             }
             count++;
         }
+
+
     }
 }

e0f186edff139612aa512740a6d2c09a32317f8f
Brad Knez <brad.knez@gmail.com>
2021-02-12 13:48:25 -0500

    Add BoomerangItem file.

---
+++ Sprintfinity3902/Entities/BoomerangItem.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using Sprintfinity3902.SpriteFactories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprintfinity3902.Entities
+{
+    class BoomerangItem : AbstractEntity
+    {
+        public BoomerangItem()
+        {
+            Sprite = ItemSpriteFactory.Instance.CreateBoomerangItem();
+        }
+
+    }
+}

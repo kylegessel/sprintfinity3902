a57260ac943c515f38475326386483f7a31a1f6b
Brad Knez <brad.knez@gmail.com>
2021-02-12 16:59:53 -0500

    Finish BoomerangItemSprite Animation.

--- Sprintfinity3902/Sprites/BoomerangItemSprite.cs
+++ Sprintfinity3902/Sprites/BoomerangItemSprite.cs
@@ -8,21 +8,16 @@ using Sprintfinity3902.Interfaces;
 
 namespace Sprintfinity3902.Sprites
 {
-    class BoomerangItemSprite : ISprite
+    class BoomerangItemSprite : AbstractSprite
     {
-        public Sprite Sprite1 { get; set; }
-        public Sprite Sprite2 { get; set; }
-        public Sprite Sprite3 { get; set; }
         public Texture2D Texture { get; set; }
-        public Animation Animation { get; set; }
-        public AnimationFrame CurrentFrame { get; set; }
 
         private const int BOOMERANG_POS1_X = 65;
         private const int BOOMERANG_POS1_Y = 189;
-        private const int BOOMERANG_WIDTH1 = 5;
+        private const int BOOMERANG_WIDTH1 = 8;
         private const int BOOMERANG_HEIGHT1 = 8;
 
-        private const int BOOMERANG_POS2_X = 74;
+        private const int BOOMERANG_POS2_X = 73;
         private const int BOOMERANG_POS2_Y = 189;
         private const int BOOMERANG_WIDTH2 = 8;
         private const int BOOMERANG_HEIGHT2 = 8;
@@ -30,64 +25,64 @@ namespace Sprintfinity3902.Sprites
         private const int BOOMERANG_POS3_X = 82;
         private const int BOOMERANG_POS3_Y = 192;
         private const int BOOMERANG_WIDTH3 = 8;
-        private const int BOOMERANG_HEIGHT3 = 5;
+        private const int BOOMERANG_HEIGHT3 = 8;
+
+        private const int BOOMERANG_POS4_X = 129;
+        private const int BOOMERANG_POS4_Y = 206;
+        private const int BOOMERANG_WIDTH4 = 8;
+        private const int BOOMERANG_HEIGHT4 = 8;
 
         public BoomerangItemSprite(Texture2D texture)
         {
             Sprite Sprite1 = new Sprite(texture, BOOMERANG_POS1_X, BOOMERANG_POS1_Y, BOOMERANG_WIDTH1, BOOMERANG_HEIGHT1);
             Sprite Sprite2 = new Sprite(texture, BOOMERANG_POS2_X, BOOMERANG_POS2_Y, BOOMERANG_WIDTH2, BOOMERANG_HEIGHT2);
             Sprite Sprite3 = new Sprite(texture, BOOMERANG_POS3_X, BOOMERANG_POS3_Y, BOOMERANG_WIDTH3, BOOMERANG_HEIGHT3);
+            Sprite Sprite4 = new Sprite(texture, BOOMERANG_POS4_X, BOOMERANG_POS4_Y, BOOMERANG_WIDTH4, BOOMERANG_HEIGHT4);
+
             Texture = texture;
 
-            GetAnimation();
-        }
-
-        public void Update(GameTime gameTime)
-        {
-            Animation.Update(gameTime);
-            CurrentFrame = Animation.CurrentFrame;
-        }
-
-        public void Draw(SpriteBatch spriteBatch, Vector2 position)
-        {
-            AnimationFrame frame1 = Animation.GetFrame(0);
-            AnimationFrame frame2 = Animation.GetFrame(1);
-            AnimationFrame frame3 = Animation.GetFrame(2);
-            AnimationFrame frame4 = Animation.GetFrame(3);
-
-            if (frame1 == CurrentFrame || frame4 == CurrentFrame)
-            {
-                Rectangle sourceRectangle = new Rectangle(Sprite1.X, Sprite1.Y, Sprite1.Width, Sprite1.Height);
-                Rectangle destinationRectangle = new Rectangle((int)position.X, (int)position.Y, 25, 40);
-                spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
-            }
-            else if (frame2 == CurrentFrame)
-            {
-                Rectangle sourceRectangle = new Rectangle(Sprite2.X, Sprite2.Y, Sprite2.Width, Sprite2.Height);
-                Rectangle destinationRectangle = new Rectangle((int)position.X, (int)position.Y, 40, 40);
-                spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
-            }
-            else if(frame3 == CurrentFrame)
-            {
-                Rectangle sourceRectangle = new Rectangle(Sprite3.X, Sprite3.Y, Sprite3.Width, Sprite3.Height);
-                Rectangle destinationRectangle = new Rectangle((int)position.X, (int)position.Y, 40, 25);
-                spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
-            }
-            else {
-                Rectangle sourceRectangle = new Rectangle(Sprite1.X, Sprite1.Y, Sprite1.Width, Sprite1.Height);
-                Rectangle destinationRectangle = new Rectangle((int)position.X, (int)position.Y, 25, 40);
-                spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
-            }
-        }
-
-        public void GetAnimation()
-        {
             Animation = new Animation();
-            Animation.AddFrame(Sprite1, 1/4f);
-            Animation.AddFrame(Sprite2, 2/4f);
-            Animation.AddFrame(Sprite3, 3/4f);
-            Animation.AddFrame(Sprite1, 4/4f);
-            Animation.Play();
+            Animation.AddFrame(Sprite1, 0 / 8f);
+            Animation.AddFrame(Sprite2, 1 / 8f);
+            Animation.AddFrame(Sprite3, 2 / 8f);
+            Animation.AddFrame(Sprite2, 3 / 8f);
+            Animation.AddFrame(Sprite1, 4 / 8f);
+            Animation.AddFrame(Sprite4, 5 / 8f);
+            Animation.AddFrame(Sprite3, 6 / 8f);
+            Animation.AddFrame(Sprite2, 7 / 8f);
+            Animation.AddFrame(Sprite1, 8/  8f);
+
         }
+
+
+        public override void Draw(SpriteBatch spriteBatch, Vector2 position)
+        {
+            if (Animation.CurrentFrame == Animation.GetFrame(3))
+            {
+                spriteBatch.Draw(Animation.CurrentFrame.Sprite.Texture, position, Animation.CurrentFrame.Sprite.SourceRectangle, Color.White, 0.0f, new Vector2(0, 0), 5.0f, SpriteEffects.FlipHorizontally, 0.0f);
+            }else if(Animation.CurrentFrame == Animation.GetFrame(4))
+            {
+                spriteBatch.Draw(Animation.CurrentFrame.Sprite.Texture, position, Animation.CurrentFrame.Sprite.SourceRectangle, Color.White, 0.0f, new Vector2(0, 0), 5.0f, SpriteEffects.FlipHorizontally, 0.0f);
+            }
+            // Skipping frame 5 and adding a new sprite, took me about 7 lines of code to get this right, feel it's just better to do the flip and change the spritesheet.
+            else if (Animation.CurrentFrame == Animation.GetFrame(6))
+            {
+                spriteBatch.Draw(Animation.CurrentFrame.Sprite.Texture, position, Animation.CurrentFrame.Sprite.SourceRectangle, Color.White, 0.0f, new Vector2(0, 0), 5.0f, SpriteEffects.FlipVertically, 0.0f);
+            }
+            else if (Animation.CurrentFrame == Animation.GetFrame(7))
+            {
+                spriteBatch.Draw(Animation.CurrentFrame.Sprite.Texture, position, Animation.CurrentFrame.Sprite.SourceRectangle, Color.White, 0.0f, new Vector2(0, 0), 5.0f, SpriteEffects.FlipVertically, 0.0f);
+            }
+            else if (Animation.CurrentFrame == Animation.GetFrame(8))
+            {
+                spriteBatch.Draw(Animation.CurrentFrame.Sprite.Texture, position, Animation.CurrentFrame.Sprite.SourceRectangle, Color.White, 0.0f, new Vector2(0, 0), 5.0f, SpriteEffects.FlipVertically, 0.0f);
+            }
+            else
+            {
+                spriteBatch.Draw(Animation.CurrentFrame.Sprite.Texture, position, Animation.CurrentFrame.Sprite.SourceRectangle, Color.White, 0.0f, new Vector2(0, 0), 5.0f, SpriteEffects.None, 0.0f);
+
+            }
+        }
+
     }
 }

51e960f3f46b02784c8ad5e2c4988d9844251e0c
Brad Knez <brad.knez@gmail.com>
2021-02-15 00:22:24 -0500

    Add explosion to BombItem.

--- Sprintfinity3902/Entities/BombItem.cs
+++ Sprintfinity3902/Entities/BombItem.cs
@@ -11,19 +11,55 @@ namespace Sprintfinity3902.Entities
     public class BombItem : AbstractEntity {
 
         Player PlayerCharacter;
-        public BombItem(Vector2 position)
+        Boolean itemUse;
+        int itemUseCount;
+        SmokeItem Smoke;
+        public BombItem(Vector2 position, SmokeItem smoke)
         {
             Sprite = ItemSpriteFactory.Instance.CreateBombItem();
             Position = position;
+            // Figure out a better implementation of the smoke to get all 5 displaying.
+            Smoke = smoke;
+            itemUse = false;
+            itemUseCount = 0;
         }
 
-        public override void Move() {
-            /*Move me*/
+        public Boolean getItemUse()
+        {
+            return itemUse;
         }
+
+        public override void Update(GameTime gameTime)
+        {
+            Sprite.Update(gameTime);
+            if (itemUse)
+            {
+                ExplodeBomb();
+            }
+        }
+
+        public void ExplodeBomb()
+        {
+            if(itemUseCount == 60)
+            {
+                Smoke.Position = new Vector2(Position.X - 8, Position.Y - 2);
+                Position = new Vector2(-1000, -1000);
+            }
+            else if(itemUseCount > 120)
+            {
+                Smoke.Sprite.Animation.Play();
+                itemUse = false;
+                itemUseCount = 0;
+                Smoke.Position = new Vector2(-1000, -1000);
+            }
+            itemUseCount++;
+        }
+
         public void UseItem(Player player)
         {
             PlayerCharacter = player;
-            //TODO: Fix math to center the bomb
+            itemUse = true;
+            
             if (PlayerCharacter.CurrentState == PlayerCharacter.facingDown)
             {
                 Position = new Vector2(player.X, player.Y + 50);

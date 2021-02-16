584a2e799df5d27a1f05fb66641ba58119b8d8fa
Ben <hobson.89@osu.edu>
2021-02-16 07:32:42 -0800

    Really hate git haha.

--- Sprintfinity3902/Game1.cs
+++ Sprintfinity3902/Game1.cs
@@ -56,6 +56,7 @@ namespace Sprintfinity3902
             Window.Title = "The Legend of Zelda";
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
             camera = new Camera(this);
 
             Graphics.ApplyChanges();

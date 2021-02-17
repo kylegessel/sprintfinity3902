9a629a991feb0c0c5ea20f9e5cf7cf645c068b42
Ben <hobson.89@osu.edu>
2021-02-16 18:48:26 -0800

    Cleaning up Animation class

--- Sprintfinity3902/Sprites/AnimationFrame.cs
+++ Sprintfinity3902/Sprites/AnimationFrame.cs
@@ -1,13 +1,13 @@
-﻿namespace Sprintfinity3902.Sprites
-{
-    public class AnimationFrame
-    {
+﻿
+
+
+namespace Sprintfinity3902.Sprites {
+    public class AnimationFrame {
+        
         private SpriteFrame _sprite;
 
-        public SpriteFrame Sprite
-        {
-            get
-            {
+        public SpriteFrame Sprite {
+            get {
                 return _sprite;
             }
             set
@@ -18,12 +18,10 @@
 
         public float TimeStamp { get; }
 
-        public AnimationFrame(SpriteFrame sprite, float timeStamp)
-        {
+        public AnimationFrame(SpriteFrame sprite, float timeStamp) {
             Sprite = sprite;
             TimeStamp = timeStamp;
         }
-
 
     }
 }

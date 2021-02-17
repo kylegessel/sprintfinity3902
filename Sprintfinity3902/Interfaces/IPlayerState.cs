382350c6ceaf6743ee22f677b529e531369f76ee
Ben <hobson.89@osu.edu>
2021-02-16 18:49:45 -0800

    I think this is up to date. I needed to put some comments in keyboard class.

---
+++ Sprintfinity3902/Interfaces/IPlayerState.cs
@@ -0,0 +1,14 @@
+﻿namespace Sprintfinity3902.Interfaces
+{
+    public interface IPlayerState
+    {
+        ISprite Sprite { get; set; }
+        void Move();
+
+        void Attack();
+
+        void UseItem();
+
+        void Update();
+    }
+}

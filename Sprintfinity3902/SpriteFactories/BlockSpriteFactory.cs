ed5b0fe1d01e14b92ac4abdf39dcb62a7a92959a
Casey Vancauwenbergh <vancauwenbergh.3@osu.edu>
2021-02-17 18:27:25 -0500

    Add remaining blocks entity and sprite classes

--- Sprintfinity3902/SpriteFactories/BlockSpriteFactory.cs
+++ Sprintfinity3902/SpriteFactories/BlockSpriteFactory.cs
@@ -25,6 +25,7 @@ namespace Sprintfinity3902.SpriteFactories
             }
         }
 
+        /* Blocks */
         public void LoadAllTextures(ContentManager content)
         {
             blockSpriteSheet = content.Load<Texture2D>(BLOCK_FILE_NAME);
@@ -34,80 +35,119 @@ namespace Sprintfinity3902.SpriteFactories
         {
             return new RegularBlockSprite(blockSpriteSheet);
         }
-
         public ISprite CreateFace1Block()
         {
             return new Face1BlockSprite(blockSpriteSheet);
         }
-
         public ISprite CreateFace2Block()
         {
             return new Face2BlockSprite(blockSpriteSheet);
         }
+        public ISprite CreateStairsBlock()
+        {
+            return new StairsBlockSprite(blockSpriteSheet);
+        }
+        public ISprite CreateBrickBlock()
+        {
+            return new BrickBlockSprite(blockSpriteSheet);
+        }
+        public ISprite CreateStripeBlock()
+        {
+            return new StripeBlockSprite(blockSpriteSheet);
+        }
+        public ISprite CreateBlackBlock()
+        {
+            return new BlackBlockSprite(blockSpriteSheet);
+        }
+        public ISprite CreateSpottedBlock()
+        {
+            return new SpottedBlockSprite(blockSpriteSheet);
+        }
+        public ISprite CreateDarkBlueBlock()
+        {
+            return new DarkBlueBlockSprite(blockSpriteSheet);
+        }
 
+        /*Doors*/
         public ISprite CreateOpenDoorLeft()
         {
-            return new OpenDoorLeftSprite();
+            return new OpenDoorLeftSprite(blockSpriteSheet);
         }
         public ISprite CreateOpenDoorRight()
         {
-            return new OpenDoorRightSprite();
+            return new OpenDoorRightSprite(blockSpriteSheet);
         }
         public ISprite CreateOpenDoorTop()
         {
-            return new OpenDoorTopSprite();
+            return new OpenDoorTopSprite(blockSpriteSheet);
         }
         public ISprite CreateOpenDoorBottom()
         {
-            return new OpenDoorBottomSprite();
+            return new OpenDoorBottomSprite(blockSpriteSheet);
         }
         public ISprite CreateClosedDoorLeft()
         {
-            return new ClosedDoorLeftSprite();
+            return new ClosedDoorLeftSprite(blockSpriteSheet);
         }
         public ISprite CreateClosedDoorRight()
         {
-            return new ClosedDoorRightSprite();
+            return new ClosedDoorRightSprite(blockSpriteSheet);
         }
         public ISprite CreateClosedDoorTop()
         {
-            return new ClosedDoorTopSprite();
+            return new ClosedDoorTopSprite(blockSpriteSheet);
         }
         public ISprite CreateClosedDoorBottom()
         {
-            return new ClosedDoorBottomSprite();
+            return new ClosedDoorBottomSprite(blockSpriteSheet);
         }
         public ISprite CreateLockedDoorLeft()
         {
-            return new LockedDoorLeftSprite();
+            return new LockedDoorLeftSprite(blockSpriteSheet);
         }
         public ISprite CreateLockedDoorRight()
         {
-            return new LockedDoorRightSprite();
+            return new LockedDoorRightSprite(blockSpriteSheet);
         }
         public ISprite CreateLockedDoorTop()
         {
-            return new LockedDoorTopSprite();
+            return new LockedDoorTopSprite(blockSpriteSheet);
         }
         public ISprite CreateLockedDoorBottom()
         {
-            return new LockedDoorBottomSprite();
+            return new LockedDoorBottomSprite(blockSpriteSheet);
         }
         public ISprite CreateHoleDoorLeft()
         {
-            return new HoleDoorLeftSprite();
+            return new HoleDoorLeftSprite(blockSpriteSheet);
         }
         public ISprite CreateHoleDoorRight()
         {
-            return new HoleDoorRightSprite();
+            return new HoleDoorRightSprite(blockSpriteSheet);
         }
         public ISprite CreateHoleDoorTop()
         {
-            return new HoleDoorTopSprite();
+            return new HoleDoorTopSprite(blockSpriteSheet);
         }
         public ISprite CreateHoleDoorBottom()
         {
-            return new HoleDoorBottomSprite();
+            return new HoleDoorBottomSprite(blockSpriteSheet);
+        }
+        public ISprite CreateWallLeft()
+        {
+            return new WallLeftSprite(blockSpriteSheet);
+        }
+        public ISprite CreateWallRight()
+        {
+            return new WallRightSprite(blockSpriteSheet);
+        }
+        public ISprite CreateWallTop()
+        {
+            return new WallTopSprite(blockSpriteSheet);
+        }
+        public ISprite CreateWallBottom()
+        {
+            return new WallBottomSprite(blockSpriteSheet);
         }
     }
 }

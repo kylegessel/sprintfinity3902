ed720b381ac5d19d11993adc00216e5bde23eec1
Bailey-Ardrey <59753201+Bailey-Ardrey@users.noreply.github.com>
2021-02-26 13:31:13 -0500

    Add position constructor to Blocks.

--- Sprintfinity3902/Entities/Blocks/StripeBlock.cs
+++ Sprintfinity3902/Entities/Blocks/StripeBlock.cs
@@ -10,5 +10,10 @@ namespace Sprintfinity3902.Entities
             Sprite = BlockSpriteFactory.Instance.CreateStripeBlock();
             Position = new Vector2(300, 700);
         }
+        public StripeBlock(Vector2 pos)
+        {
+            Sprite = BlockSpriteFactory.Instance.CreateStripeBlock();
+            Position = pos;
+        }
     }
 }

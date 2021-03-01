04247b1298d2a3aa41d3bf86de1fd38eadd900e1
Brad Knez <brad.knez@gmail.com>
2021-03-01 02:01:12 -0500

    Update RoomLoader to use new Door state pattern.

--- Sprintfinity3902/Maps/RoomLoader.cs
+++ Sprintfinity3902/Maps/RoomLoader.cs
@@ -1,9 +1,8 @@
 ﻿using System.IO;
-using System.Collections.Generic;
 using Sprintfinity3902.Interfaces;
-using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
 using Sprintfinity3902.Entities;
+using Sprintfinity3902.Entities.Doors;
 
 namespace Sprintfinity3902.Maps
 {
@@ -11,6 +10,13 @@ namespace Sprintfinity3902.Maps
     {
         StreamReader mapStream;
         private IRoom Room { get; set; }
+        public Door DoorTop { get; set; }
+        public Door DoorBottom { get; set; }
+        public Door DoorLeft { get; set; }
+        public Door DoorRight { get; set; }
+
+
+
 
         // Have this input a filename and then load the room.
         public RoomLoader(IRoom room)
@@ -23,22 +29,30 @@ namespace Sprintfinity3902.Maps
 
         public void Build()
         {
+            DoorTop = new Door(new Vector2(560, 320));
+            DoorBottom = new Door(new Vector2(560, 1040));
+            DoorLeft = new Door(new Vector2(0, 680));
+            DoorRight = new Door(new Vector2(1120, 680));
             Room.roomEntities.Add(new RoomExterior(new Vector2(0, 320)));
             Room.roomEntities.Add(new RoomInterior(new Vector2(160, 480)));
+            Room.roomEntities.Add(DoorTop);
+            Room.roomEntities.Add(DoorBottom);
+            Room.roomEntities.Add(DoorLeft);
+            Room.roomEntities.Add(DoorRight);
             
             string line;
             int currX = 160;
             int currY = 480;
             Vector2 Position = new Vector2(currX, currY);
-            while (!mapStream.EndOfStream)
+            for(int i = 0; i < 7; i++)
             {
                 line = mapStream.ReadLine();
                 if (!string.IsNullOrWhiteSpace(line))
                 {
                     string[] lineValues = line.Split(',');
-                    foreach (string s in lineValues)
+                    for(int j = 0; j < 12; j++)
                     {
-                        switch (s)
+                        switch (lineValues[j])
                         {
                             //BLOCKS
                             case "TILE":
@@ -82,7 +96,33 @@ namespace Sprintfinity3902.Maps
                     }
                 }
             }
-            
+            for(int i = 0; i < 4; i++)
+            {
+                line = mapStream.ReadLine();
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    string[] lineValues = line.Split(',');
+                    switch (lineValues[0])
+                    {
+                        case "WALT":
+                            DoorTop.SetState(DoorBottom.wallTop);
+                            break;
+                        case "WALB":
+                            DoorBottom.SetState(DoorBottom.wallBottom);
+                            break;
+                        case "WALL":
+                            DoorLeft.SetState(DoorBottom.wallLeft);
+                            break;
+                        case "WALR":
+                            DoorRight.SetState(DoorBottom.wallRight);
+                            break;
+                        case "ODRT":
+                            DoorTop.SetState(DoorTop.openDoorTop);
+                            break;
+                    }
+                }
+
+            }
         }
             
     }

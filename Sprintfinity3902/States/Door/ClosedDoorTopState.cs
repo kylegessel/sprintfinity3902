c554ea69c27927f5710e6fc50633d9f408baa9dc
Brad Knez <brad.knez@gmail.com>
2021-03-01 14:28:08 -0500

    Add all door states.

---
+++ Sprintfinity3902/States/Door/ClosedDoorTopState.cs
@@ -0,0 +1,29 @@
+﻿using Sprintfinity3902.Interfaces;
+using Sprintfinity3902.SpriteFactories;
+
+namespace Sprintfinity3902.States.Door
+{
+    class ClosedDoorTopState : IDoorState
+    {
+
+        public Entities.Doors.Door CurrentDoor { get; set; }
+        public ISprite Sprite { get; set; }
+
+        public ClosedDoorTopState(Entities.Doors.Door currentDoor)
+        {
+            CurrentDoor = currentDoor;
+            Sprite = BlockSpriteFactory.Instance.CreateClosedDoorTop();
+        }
+
+        public void Open()
+        {
+            //NULL
+        }
+
+        // To be implemented when room first entered, door starts opened then closes.
+        public void Close()
+        {
+            //NULL
+        }
+    }
+}
---
+++ Sprintfinity3902/States/Door/LockedDoorBottomState.cs
@@ -0,0 +1,29 @@
+﻿using Sprintfinity3902.Interfaces;
+using Sprintfinity3902.SpriteFactories;
+
+namespace Sprintfinity3902.States.Door
+{
+    class LockedDoorBottomState : IDoorState
+    {
+
+        public Entities.Doors.Door CurrentDoor { get; set; }
+        public ISprite Sprite { get; set; }
+
+        public LockedDoorBottomState(Entities.Doors.Door currentDoor)
+        {
+            CurrentDoor = currentDoor;
+            Sprite = BlockSpriteFactory.Instance.CreateLockedDoorBottom();
+        }
+
+        public void Open()
+        {
+            //NULL
+        }
+
+        // To be implemented when room first entered, door starts opened then closes.
+        public void Close()
+        {
+            //NULL
+        }
+    }
+}

4d9394fc2b647e4166daa183699324d38aeaaec5
Brad Knez <brad.knez@gmail.com>
2021-03-01 02:00:58 -0500

    Add door state pattern.

---
+++ Sprintfinity3902/Entities/Doors/Door.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Sprintfinity3902.Interfaces;
+using Sprintfinity3902.States.Door;
+
+namespace Sprintfinity3902.Entities.Doors
+{
+    public class Door : AbstractEntity
+    {
+        private IDoorState _currentState;
+
+        public IDoorState CurrentState
+        {
+            get
+            {
+                return _currentState;
+            }
+            set
+            {
+                _currentState = value;
+            }
+        }
+
+        public IDoorState wallTop { get; set; }
+        public IDoorState wallBottom { get; set; }
+        public IDoorState wallLeft { get; set; }
+        public IDoorState wallRight { get; set; }
+        public IDoorState openDoorTop { get; set; }
+
+
+
+
+        public Door(Vector2 position)
+        {
+            Position = position;
+            CurrentState = new WallTopState(this);
+            wallTop = CurrentState;
+            wallBottom = new WallBottomState(this);
+            wallLeft = new WallLeftState(this);
+            wallRight = new WallRightState(this);
+            openDoorTop = new OpenDoorTopState(this);
+            
+        }
+
+        public void SetState(IDoorState state)
+        {
+            Vector2 pos = Position;
+            CurrentState = state;
+            Position = pos;
+        }
+
+        public void Open()
+        {
+            CurrentState.Open();
+        }
+
+        public void Close()
+        {
+            CurrentState.Close();
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            CurrentState.Sprite.Draw(spriteBatch, Position, Color.White);
+        }
+        public override void Update(GameTime gameTime)
+        {
+            CurrentState.Sprite.Update(gameTime);
+        }
+    }
+}
+
---
+++ Sprintfinity3902/States/Door/WallBottomState.cs
@@ -0,0 +1,28 @@
+﻿using Sprintfinity3902.Interfaces;
+using Sprintfinity3902.SpriteFactories;
+
+namespace Sprintfinity3902.States.Door
+{
+    class WallBottomState : IDoorState
+    {
+
+        public Entities.Doors.Door CurrentDoor { get; set; }
+        public ISprite Sprite { get; set; }
+
+        public WallBottomState(Entities.Doors.Door currentDoor)
+        {
+            CurrentDoor = currentDoor;
+            Sprite = BlockSpriteFactory.Instance.CreateWallBottom();
+        }
+
+        public void Open()
+        {
+            //NULL
+        }
+
+        public void Close()
+        {
+            //NULL
+        }
+    }
+}

1f986a87b30563c83b6212c94ed63b95a583224e
Brad Knez <brad.knez@gmail.com>
2021-03-14 21:25:37 -0400

    Add moving sword item.

--- Sprintfinity3902/Entities/Items/MovingSwordItem.cs
+++ Sprintfinity3902/Entities/Items/MovingSwordItem.cs
@@ -127,8 +127,8 @@ namespace Sprintfinity3902.Entities
         public Boolean Collide(int enemyID, IEnemy enemy, IRoom room)
         {
             // Code for removing sword on contact, needs to be replaced.
+            room.garbage.Add(new MovingSwordSplitItem(Position));
             Position = new Vector2(-1000, -1000);
-            CollisionDetector.decorateList.Add(enemyID);
             return enemy.HitRegister(enemyID, 1, 0, swordDirection, room) <= 0;
         }
     }
---
+++ Sprintfinity3902/Sprites/Items/SwordSplitBottomRightSprite.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Sprintfinity3902.Sprites.Items
+{
+    class SwordSplitBottomRightSprite : AbstractSprite
+    {
+        public Texture2D Texture { get; set; }
+
+
+        private const int SPLIT_X1 = 142;
+        private const int SPLIT_Y1 = 205;
+        private const int SPLIT_WIDTH1 = 10;
+        private const int SPLIT_HEIGHT1 = 10;
+
+        private const int SPLIT_X2 = 156;
+        private const int SPLIT_Y2 = 205;
+        private const int SPLIT_WIDTH2 = 10;
+        private const int SPLIT_HEIGHT2 = 10;
+
+        public SwordSplitBottomRightSprite(Texture2D texture)
+        {
+
+            SpriteFrame Sprite1 = new SpriteFrame(texture, SPLIT_X1, SPLIT_Y1, SPLIT_WIDTH1, SPLIT_HEIGHT1);
+            SpriteFrame Sprite2 = new SpriteFrame(texture, SPLIT_X2, SPLIT_Y2, SPLIT_WIDTH2, SPLIT_HEIGHT2);
+
+            Animation = new Animation();
+            Animation.AddFrame(Sprite1, 0);
+            Animation.AddFrame(Sprite2, 1 / 32f);
+            Animation.AddFrame(Sprite1, 1 / 16f);
+        }
+    }
+}

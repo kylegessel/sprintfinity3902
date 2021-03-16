ebf3d3ff061932dfe6481e0cc1ae93d947af4c7d
Brad Knez <brad.knez@gmail.com>
2021-03-15 20:41:33 -0400

    Change CheckSpecificCollision to return.

--- Sprintfinity3902/Collision/CollisionDetector.cs
+++ Sprintfinity3902/Collision/CollisionDetector.cs
@@ -202,14 +202,7 @@ namespace Sprintfinity3902.Collision
         public bool CheckSpecificCollision(Rectangle rec)
         {
             Rectangle linkRect = link.GetBoundingRect();
-            if (rec.Intersects(linkRect))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return rec.Intersects(linkRect);
         }
 
     }

d4f0035afed923cb52e75e684ed4f44d78d8ae30
Bailey-Ardrey <59753201+Bailey-Ardrey@users.noreply.github.com>
2021-03-17 22:38:56 -0400

    Create Hud Sprites and add them to the HudSpriteFactory.

--- Sprintfinity3902/SpriteFactories/HudSpriteFactory.cs
+++ Sprintfinity3902/SpriteFactories/HudSpriteFactory.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Sprintfinity3902.Interfaces;
+using Sprintfinity3902.Sprites;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -32,5 +33,19 @@ namespace Sprintfinity3902.SpriteFactories
             hudSpriteSheet = content.Load<Texture2D>(HUD_FILE_NAME);
         }
 
+        public ISprite CreateInGameHud()
+        {
+            return new InGameHudSprite(hudSpriteSheet);
+        }
+
+        public ISprite CreateDungeonHud()
+        {
+            return new InGameHudSprite(hudSpriteSheet);
+        }
+
+        public ISprite CreateInventoryHud()
+        {
+            return new InventoryHudSprite(hudSpriteSheet);
+        }
     }
 }
---
+++ Sprintfinity3902/Sprites/HudElements/DungeonHudSprite.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprintfinity3902.Sprites
+{
+    public class DungeonHudSprite : AbstractSprite
+    {
+        public Texture2D Texture { get; set; }
+
+        private const int HUD_X = 258;
+        private const int HUD_Y = 112;
+        private const int HUD_WIDTH = 256;
+        private const int HUD_HEIGHT = 88;
+
+        public DungeonHudSprite(Texture2D texture)
+        {
+            SpriteFrame sprite1 = new SpriteFrame(texture, HUD_X, HUD_Y, HUD_WIDTH, HUD_HEIGHT);
+            Texture = texture;
+
+            Animation = new Animation();
+            Animation.AddFrame(sprite1, 0);
+        }
+    }
+}

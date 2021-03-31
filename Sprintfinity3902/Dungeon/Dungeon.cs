728d5a997427f0ed18dcd734df8fd9d15065195c
Kyle Gessel <44045020+kylegessel@users.noreply.github.com>
2021-03-30 21:14:36 -0400

    Fix room 16 minimap bug.

--- Sprintfinity3902/Dungeon/Dungeon.cs
+++ Sprintfinity3902/Dungeon/Dungeon.cs
@@ -88,10 +88,10 @@ namespace Sprintfinity3902.Dungeon
             {
                 rload.Initialize(room);
                 rload.Build();
-                if (room.Id != 13)
-                { 
+                //if (room.Id != 13)
+                //{ 
                     RoomLocations.Add(room.RoomPos);
-                }
+                //}
                 //RoomLocations.Add(room.RoomPos);
             }
         }

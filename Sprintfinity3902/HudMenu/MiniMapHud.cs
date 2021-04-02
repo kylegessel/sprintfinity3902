ece3d508c230992f695af3967f2426a840b70d48
Kyle Gessel <44045020+kylegessel@users.noreply.github.com>
2021-04-02 11:34:26 -0400

    Update to build minimap upon pickup.

--- Sprintfinity3902/HudMenu/MiniMapHud.cs
+++ Sprintfinity3902/HudMenu/MiniMapHud.cs
@@ -12,23 +12,34 @@ namespace Sprintfinity3902.HudMenu
         private IPlayer Link;
         private HudInitializer hudInitializer;
         private List<Point> RoomLocations;
+        private bool MapPickup;
 
         public List<IEntity> Icons { get; private set; }
+        public List<IEntity> Map { get; private set; }
 
         public MiniMapHud(Game1 game, List<Point> roomLocations)
         {
             Game = game;
             Link = Game.playerCharacter;
             Icons = new List<IEntity>();
+            Map = new List<IEntity>();
             RoomLocations = roomLocations;
             hudInitializer = new HudInitializer(this);
+            MapPickup = false;
 
             Initialize();
         }
 
         public void Update(GameTime gameTime)
         {
-
+            if (!MapPickup && Link.itemcount[IItem.ITEMS.MAP] > 0)
+            {
+                foreach (IEntity room in Map)
+                {
+                    Icons.Add(room);
+                }
+                MapPickup = true;
+            }
         }
 
         public void Draw(SpriteBatch spriteBatch, Color color)
@@ -36,11 +47,12 @@ namespace Sprintfinity3902.HudMenu
             foreach (IEntity icon in Icons) {
                 icon.Draw(spriteBatch, color);
             }
+           
         }
 
         public void Initialize()
         {
-            hudInitializer.InitializeMiniMap(RoomLocations);
+            hudInitializer.InitializeMiniMap(RoomLocations, Map);
         }
     }
 }

d9be5c854045624d9a33ec458852cdeeb8da4c2a
Kyle Gessel <44045020+kylegessel@users.noreply.github.com>
2021-04-09 11:30:53 -0400

    Add template rooms

---
+++ Sprintfinity3902/Dungeon/Room - Copy.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Sprintfinity3902.Interfaces;
+using System.Collections.Generic;
+
+namespace Sprintfinity3902.Dungeon
+{
+    public class DungeonGenerator
+    {
+        
+
+    }
+}

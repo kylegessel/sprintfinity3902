c850e7aa1257ca822fa3b6c67bc9a3a1b2d1c8f3
Kyle Gessel <44045020+kylegessel@users.noreply.github.com>
2021-04-19 01:25:51 -0400

    Update room Id checks.

--- Sprintfinity3902/Dungeon/DungeonGenerator.cs
+++ Sprintfinity3902/Dungeon/DungeonGenerator.cs
@@ -60,52 +60,50 @@ namespace Sprintfinity3902.Dungeon
 
                 if (RoomLocations.Contains(new Point(room.X + 1,room.Y)))
                 {
-                    id = LocationId.GetValueOrDefault(new Point(room.X + 1, room.Y));
-                    File.AppendAllText(@"..\..\..\Content\GeneratedRooms\GenRoom" + j + ".csv", "ODRR, "+ id +",,,,,,,,,,,\n");
+                    nextId = LocationId.GetValueOrDefault(new Point(room.X + 1, room.Y));
+                    File.AppendAllText(@"..\..\..\Content\GeneratedRooms\GenRoom" + id + ".csv", "ODRR, "+ nextId +",,,,,,,,,,,\n");
                 }
                 else
                 {
-                    File.AppendAllText(@"..\..\..\Content\GeneratedRooms\GenRoom" + j + ".csv", "WALR, -1,,,,,,,,,,\n");
+                    File.AppendAllText(@"..\..\..\Content\GeneratedRooms\GenRoom" + id + ".csv", "WALR, -1,,,,,,,,,,\n");
                 }
 
                 if (RoomLocations.Contains(new Point(room.X - 1, room.Y)))
                 {
-                    id = LocationId.GetValueOrDefault(new Point(room.X - 1, room.Y));
-                    File.AppendAllText(@"..\..\..\Content\GeneratedRooms\GenRoom" + j + ".csv", "ODRL, " + id + ",,,,,,,,,,,\n");
+                    nextId = LocationId.GetValueOrDefault(new Point(room.X - 1, room.Y));
+                    File.AppendAllText(@"..\..\..\Content\GeneratedRooms\GenRoom" + id + ".csv", "ODRL, " + nextId + ",,,,,,,,,,,\n");
                 }
                 else
                 {
-                    File.AppendAllText(@"..\..\..\Content\GeneratedRooms\GenRoom" + j + ".csv", "WALL, -1,,,,,,,,,,\n");
+                    File.AppendAllText(@"..\..\..\Content\GeneratedRooms\GenRoom" + id + ".csv", "WALL, -1,,,,,,,,,,\n");
                 }
 
                 if (RoomLocations.Contains(new Point(room.X, room.Y + 1)))
                 {
-                    id = LocationId.GetValueOrDefault(new Point(room.X, room.Y+1));
-                    File.AppendAllText(@"..\..\..\Content\GeneratedRooms\GenRoom" + j + ".csv", "ODRB, " + id + ",,,,,,,,,,,\n");
+                    nextId = LocationId.GetValueOrDefault(new Point(room.X, room.Y+1));
+                    File.AppendAllText(@"..\..\..\Content\GeneratedRooms\GenRoom" + id + ".csv", "ODRB, " + nextId + ",,,,,,,,,,,\n");
                 }
                 else
                 {
-                    File.AppendAllText(@"..\..\..\Content\GeneratedRooms\GenRoom" + j + ".csv", "WALB, -1,,,,,,,,,,\n");
+                    File.AppendAllText(@"..\..\..\Content\GeneratedRooms\GenRoom" + id + ".csv", "WALB, -1,,,,,,,,,,\n");
                 }
 
                 if (RoomLocations.Contains(new Point(room.X, room.Y-1)))
                 {
-                    id = LocationId.GetValueOrDefault(new Point(room.X, room.Y-1));
-                    File.AppendAllText(@"..\..\..\Content\GeneratedRooms\GenRoom" + j + ".csv", "ODRT, " + id + ",,,,,,,,,,,\n");
+                    nextId = LocationId.GetValueOrDefault(new Point(room.X, room.Y-1));
+                    File.AppendAllText(@"..\..\..\Content\GeneratedRooms\GenRoom" + id + ".csv", "ODRT, " + nextId + ",,,,,,,,,,,\n");
                 }
                 else
                 {
-                    File.AppendAllText(@"..\..\..\Content\GeneratedRooms\GenRoom" + j + ".csv", "WALT, -1,,,,,,,,,,\n");
+                    File.AppendAllText(@"..\..\..\Content\GeneratedRooms\GenRoom" + id + ".csv", "WALT, -1,,,,,,,,,,\n");
                 }
 
 
 
 
 
-                File.AppendAllText(@"..\..\..\Content\GeneratedRooms\GenRoom" + j + ".csv", room.X + "," + room.Y + ",,,,,,,,,,,\n");
-                File.AppendAllText(@"..\..\..\Content\GeneratedRooms\GenRoom" + j + ".csv", "1\n");
-
-                j++;
+                File.AppendAllText(@"..\..\..\Content\GeneratedRooms\GenRoom" + id + ".csv", room.X + "," + room.Y + ",,,,,,,,,,,\n");
+                File.AppendAllText(@"..\..\..\Content\GeneratedRooms\GenRoom" + id + ".csv", "1\n");
             }
 
             return RoomLocations.Count + FinalRooms.Count + 1;
@@ -188,5 +186,7 @@ namespace Sprintfinity3902.Dungeon
 
             }
         }
+
+        
     }
 }

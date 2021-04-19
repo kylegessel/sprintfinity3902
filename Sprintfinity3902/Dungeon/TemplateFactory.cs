f0f10b5a3e718990726b7c9500d49e4149304003
Ben <hobson.89@osu.edu>
2021-04-19 05:40:12 -0700

    Update

--- Sprintfinity3902/Dungeon/TemplateFactory.cs
+++ Sprintfinity3902/Dungeon/TemplateFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Sprintfinity3902.Dungeon
@@ -7,9 +8,10 @@ namespace Sprintfinity3902.Dungeon
     public class TemplateFactory
     {
 
-        private static TemplateFactory instance;
 
-        public static TemplateFactory Instance {
+        private static TemplateFactory instance;
+        public static TemplateFactory Instance
+        {
             get {
                 if (instance == null) {
                     instance = new TemplateFactory();
@@ -22,6 +24,22 @@ namespace Sprintfinity3902.Dungeon
 
         }
 
+        public void Initialize() { }
+
+        public void CreateTemplateWithParams(string path=@"..\..\..\Content\GeneratedRooms\DefaultOut.csv") {
+            File.Create(path);
+            File.AppendAllText(path, "RMEX,,,,,,,,,,,,");
+            File.AppendAllText(path, "RMIN,,,,,,,,,,,,");
+            File.AppendAllText(path, @" , , , , , , , , , , , , 
+ , , , , , , , , , , , ,
+ , , , , ,WLD1, , , , , , ,
+ , , , , , , , , , , , ,
+ , , , , , , , , , , , ,
+ , , , ,WLD1, , , , , , , ,
+ , , , , , , , , , , , , 
+");
+            
+        }
 
     }
 }

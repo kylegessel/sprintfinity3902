418c9c17a57a06690f1439519d466dd8d4fc3900
Ben <hobson.89@osu.edu>
2021-04-19 05:28:11 -0700

    Created TemplateFactory Singleton

---
+++ Sprintfinity3902/Dungeon/TemplateFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprintfinity3902.Dungeon
+{
+    public class TemplateFactory
+    {
+
+        private static TemplateFactory instance;
+
+        public static TemplateFactory Instance {
+            get {
+                if (instance == null) {
+                    instance = new TemplateFactory();
+                }
+                return instance;
+            }
+        }
+
+        private TemplateFactory() { 
+
+        }
+
+
+    }
+}
